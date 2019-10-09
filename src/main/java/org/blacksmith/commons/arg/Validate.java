3d052a117bce806a0ea67246b33dbf8efd8dc6f4
januszkowal <janusz.kowal@interia.pl>
2019-10-09 20:33:17 +0200

    Update Validate.java

--- src/main/java/org/blacksmith/commons/arg/Validate.java
+++ src/main/java/org/blacksmith/commons/arg/Validate.java
@@ -11,82 +11,82 @@ public class Validate {
    * @param o Object to be checked.
    * @throws IllegalArgumentException if {@code o} is {@code null}.
    */
-  public static void checkNotNull(Object o) {
+  public static void notNull(Object o) {
     if (o == null) {
       throw new IllegalArgumentException("Argument can't be null");
     }
   }
-  public static void checkNotNull(Object o, String message) {
+  public static void notNull(Object o, String message) {
     if (o == null) {
       throw new IllegalArgumentException(message);
     }
   }
-  public static void checkNotNull(Object o, Supplier<String> messageSupplier) {
+  public static void notNull(Object o, Supplier<String> messageSupplier) {
     if (o == null) {
       throw new IllegalArgumentException(messageSupplier.get());
     }
   }
 
-  public static void checkNotEmpty(Object[] a) {
+  public static void notEmpty(Object[] a) {
     if (a==null || a.length == 0) {
       throw new IllegalArgumentException("Array can't be empty or null");
     }
   }
-  public static void checkNotEmpty(Collection<?> c) {
+  public static void notEmpty(Collection<?> c) {
     if (c==null || c.isEmpty()) {
       throw new IllegalArgumentException("Collection can't be empty or null");
     }
   }
 
-  public static void checkNotEmpty(Object[] a, String message) {
+  public static void notEmpty(Object[] a, String message) {
     if (a==null || a.length == 0) {
       throw new IllegalArgumentException(message);
     }
   }
-  public static void checkNotEmpty(Collection<?> c, String message) {
+  public static void notEmpty(Collection<?> c, String message) {
     if (c == null || c.isEmpty()) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  public static void checkNotEmpty(Object[] a, Supplier<String> messageSupplier) {
+  public static void notEmpty(Object[] a, Supplier<String> messageSupplier) {
     if (a==null || a.length == 0) {
       throw new IllegalArgumentException(messageSupplier.get());
     }
   }
-  public static void checkNotEmpty(Collection<?> c, Supplier<String> messageSupplier) {
+  public static void notEmpty(Collection<?> c, Supplier<String> messageSupplier) {
     if (c==null || c.isEmpty()) {
       throw new IllegalArgumentException(messageSupplier.get());
     }
   }
 
-  public static <T extends Comparable> void inOrder(T val1, T val2, String message) {
-    checkNotNull(val1);
-    checkNotNull(val2);
+  public static <T extends Comparable<T>> void inOrder(T val1, T val2, String message) {
+    notNull(val1);
+    notNull(val2);
     if (val1.compareTo(val2)>0) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  public static <T extends Comparable> void inOrder(T val1, T val2, Supplier<String> messageSupplier) {
-    checkNotNull(val1);
-    checkNotNull(val2);
+  public static <T extends Comparable<T>> void inOrder(T val1, T val2, Supplier<String> messageSupplier) {
+    notNull(val1);
+    notNull(val2);
     if (val1.compareTo(val2)>=0) {
       throw new IllegalArgumentException(messageSupplier.get());
     }
   }
 
-  public static <T extends Comparable> void inOrderOrEqual(T val1, T val2, String message) {
-    checkNotNull(val1);
-    checkNotNull(val2);
+  public static <T extends Comparable<T>> void inOrderOrEqual(T val1, T val2, String message) {
+    notNull(val1);
+    notNull(val2);
     if (val1.compareTo(val2)>=0) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  public static <T extends Comparable> void inOrderOrEqual(T val1, T val2, Supplier<String> messageSupplier) {
-    checkNotNull(val1);
-    checkNotNull(val2);
+  public static <T extends Comparable<T>> void inOrderOrEqual(T val1, T val2, Supplier<String> messageSupplier) {
+    notNull(val1);
+    notNull(val2);
     if (val1.compareTo(val2)>0) {
       throw new IllegalArgumentException(messageSupplier.get());
     }
