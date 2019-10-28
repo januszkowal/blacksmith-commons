c64bbce749d78896e38b7ded90fa8ec7f8b3b2e2
Janusz Kowal <jkowalrz@gmail.com>
2019-10-28 13:15:14 +0100

    Reflection utils added

---
+++ src/main/java/org/blacksmith/commons/reflection/ReflectionUtils.java
@@ -0,0 +1,19 @@
+package org.blacksmith.commons.reflection;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+public class ReflectionUtils {
+  public static ParameterizedType findByRawType(Type[] genericInterfaces, Class<?> expectedRawType)
+  {
+    for (Type type : genericInterfaces) {
+      if (type instanceof ParameterizedType) {
+        ParameterizedType parametrized = (ParameterizedType) type;
+        if (expectedRawType.equals(parametrized.getRawType())) {
+          return parametrized;
+        }
+      }
+    }
+    throw new RuntimeException();
+  }
+}
