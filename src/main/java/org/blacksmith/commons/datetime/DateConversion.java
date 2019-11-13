d4234791fe63707cfad58adaf5c973a792bd898c
Janusz Kowal <jkowalrz@gmail.com>
2019-11-13 13:39:52 +0100

    Added date conversion methods

--- src/main/java/org/blacksmith/commons/datetime/DateConversion.java
+++ src/main/java/org/blacksmith/commons/datetime/DateConversion.java
@@ -1,13 +1,66 @@
 package org.blacksmith.commons.datetime;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 
 public class DateConversion {
-  private DateConversion() {}
-  public static java.util.Date convertLocalDateToDate(LocalDate date) {
-    return java.util.Date.from(date.atStartOfDay()
-        .atZone(ZoneId.systemDefault())
-        .toInstant());
+  private DateConversion() {
+  }
+
+  /* Via instant*/
+  public static Date convertLocalDateToDate(LocalDate localDate) {
+    return java.util.Date.from(localDate.atStartOfDay()
+        .atZone(ZoneId.systemDefault()).toInstant());
+  }
+
+  /* Via SqlDate */
+  public Date convertLocalDateToDateViaSqlDate(LocalDate localDate) {
+    return java.sql.Date.valueOf(localDate);
+  }
+
+  /* Via Instant */
+  public static Date convertLocalDateTimeToDate(LocalDateTime localDateTime) {
+    return java.util.Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+  }
+
+  public Date convertLocalDateTimeToDateViaSqlTimestamp(LocalDateTime localDateTime) {
+    return java.sql.Timestamp.valueOf(localDateTime);
+  }
+
+  /* Via instant*/
+  public static LocalDate convertDateToLocalDate(Date date) {
+    return date.toInstant()
+        .atZone(ZoneId.systemDefault()).toLocalDate();
+  }
+
+  /* via milisecond */
+  public static LocalDate convertDateToLocalDateViaMilisecond(Date date) {
+    return Instant.ofEpochMilli(date.getTime())
+        .atZone(ZoneId.systemDefault()).toLocalDate();
+  }
+
+  /* Via SqlDate */
+  public LocalDate convertDateToLocalDateViaSqlDate(Date date) {
+    return new java.sql.Date(date.getTime()).toLocalDate();
+  }
+
+  /* Via instant*/
+  public LocalDateTime convertToLocalDateTime(Date date) {
+    return date.toInstant()
+        .atZone(ZoneId.systemDefault()).toLocalDateTime();
+  }
+
+  /* via milisecond */
+  public static LocalDateTime convertDateToLocalDateTimeViaMilisecond(Date date) {
+    return Instant.ofEpochMilli(date.getTime())
+        .atZone(ZoneId.systemDefault()).toLocalDateTime();
+  }
+
+  /* Via SqlTimestamp */
+  public LocalDateTime convertDateToLocalDateTimeViaSqlTimestamp(Date date) {
+    return new java.sql.Timestamp(date.getTime()).toLocalDateTime();
   }
 }
