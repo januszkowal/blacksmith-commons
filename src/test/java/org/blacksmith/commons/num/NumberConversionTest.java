6fb26ddfea66db084b138d34861fdec68e1987af
Janusz Kowal <jkowalrz@gmail.com>
2020-05-27 20:15:03 +0200

    NumberConversion test

--- src/test/java/org/blacksmith/commons/num/NumberConversionTest.java
+++ src/test/java/org/blacksmith/commons/num/NumberConversionTest.java
@@ -20,4 +20,17 @@ class NumberConversionTest {
   void bigDecimalFromDoubleTest() {
     Assertions.assertEquals(BigDecimal.valueOf(89.999993d),NumberConversion.doubleToBigDecimal(89.999993d));
   }
+
+  @Test
+  void isNumberTest() {
+    //objects
+    Assertions.assertTrue(NumberConversion.isNumber(Double.valueOf(2.8)));
+    Assertions.assertTrue(NumberConversion.isNumber(Integer.valueOf(2)));
+    Assertions.assertTrue(NumberConversion.isNumber(BigDecimal.valueOf(2.8)));
+    //primitives
+    Assertions.assertTrue(NumberConversion.isNumber(2.8));
+    Assertions.assertTrue(NumberConversion.isNumber(2));
+    Assertions.assertTrue(NumberConversion.isNumber(2f));
+    Assertions.assertTrue(NumberConversion.isNumber(2L));
+  }
 }
