551f484d604d583179b7415bebf48b920e6fdb40
Janusz Kowal <jkowalrz@gmail.com>
2020-06-23 14:46:49 +0200

    DateUtils isValidDate/min/max test added

---
+++ src/test/java/org/blacksmith/commons/datetime/DateUtilsTest.java
@@ -0,0 +1,35 @@
+package org.blacksmith.commons.datetime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+class DateUtilsTest {
+  @Test
+  public void isValidDate() {
+    assertTrue(DateUtils.isValidDate(2019,1,28));
+    assertTrue(DateUtils.isValidDate(2019,1,29));
+    assertTrue(DateUtils.isValidDate(2019,1,30));
+    assertTrue(DateUtils.isValidDate(2019,1,31));
+    assertFalse(DateUtils.isValidDate(2019,1,32));
+
+    assertTrue(DateUtils.isValidDate(2019,2,28));
+    assertFalse(DateUtils.isValidDate(2019,2,29));
+    assertFalse(DateUtils.isValidDate(2019,2,30));
+
+    assertTrue(DateUtils.isValidDate(2020,2,28));
+    assertTrue(DateUtils.isValidDate(2020,2,29));
+    assertFalse(DateUtils.isValidDate(2020,2,30));
+  }
+  @Test
+  public void min() {
+    assertEquals(LocalDate.parse("2020-01-01"),DateUtils.min(LocalDate.parse("2020-01-01"),LocalDate.parse("2020-01-02")));
+    assertEquals(LocalDate.parse("2020-01-01"),DateUtils.min(LocalDate.parse("2020-01-02"),LocalDate.parse("2020-01-01")));
+  }
+  @Test
+  public void max() {
+    assertEquals(LocalDate.parse("2020-01-02"),DateUtils.max(LocalDate.parse("2020-01-01"),LocalDate.parse("2020-01-02")));
+    assertEquals(LocalDate.parse("2020-01-02"),DateUtils.max(LocalDate.parse("2020-01-02"),LocalDate.parse("2020-01-01")));
+  }
+}
