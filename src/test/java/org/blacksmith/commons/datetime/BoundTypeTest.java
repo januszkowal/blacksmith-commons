ae77c9b92e7d26b4c5cd0bdb5fc1f2f79dd6a0f9
Janusz Kowal <jkowalrz@gmail.com>
2020-06-23 20:52:00 +0200

    BoundType test

---
+++ src/test/java/org/blacksmith/commons/datetime/BoundTypeTest.java
@@ -0,0 +1,14 @@
+package org.blacksmith.commons.datetime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoundTypeTest {
+
+  @Test
+  void flip() {
+    assertEquals(BoundType.OPEN,BoundType.CLOSED.flip());
+    assertEquals(BoundType.CLOSED,BoundType.OPEN.flip());
+  }
+}
