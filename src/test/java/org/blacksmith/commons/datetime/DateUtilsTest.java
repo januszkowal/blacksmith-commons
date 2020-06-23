06252483eb9cd13c784f9666d02c0201ec0c1c0e
Janusz Kowal <jkowalrz@gmail.com>
2020-06-23 15:05:39 +0200

    DateUtils min/max fixed

--- src/test/java/org/blacksmith/commons/datetime/DateUtilsTest.java
+++ src/test/java/org/blacksmith/commons/datetime/DateUtilsTest.java
@@ -26,12 +26,14 @@ class DateUtilsTest {
   public void min() {
     assertEquals(LocalDate.parse("2020-01-01"),DateUtils.min(LocalDate.parse("2020-01-01"),LocalDate.parse("2020-01-02")));
     assertEquals(LocalDate.parse("2020-01-01"),DateUtils.min(LocalDate.parse("2020-01-02"),LocalDate.parse("2020-01-01")));
-    assertEquals(LocalDate.parse("2003-01-15"),DateUtils.min(LocalDate.parse("2003-01-15"),LocalDate.parse("2003-07-15")));
-    assertEquals(LocalDate.parse("2003-01-15"),DateUtils.min(LocalDate.parse("2003-07-15"),LocalDate.parse("2003-01-15")));
+    assertEquals(LocalDate.parse("2003-01-10"),DateUtils.min(LocalDate.parse("2003-01-10"),LocalDate.parse("2003-07-15")));
+    assertEquals(LocalDate.parse("2003-01-10"),DateUtils.min(LocalDate.parse("2003-07-15"),LocalDate.parse("2003-01-10")));
   }
   @Test
   public void max() {
     assertEquals(LocalDate.parse("2020-01-02"),DateUtils.max(LocalDate.parse("2020-01-01"),LocalDate.parse("2020-01-02")));
     assertEquals(LocalDate.parse("2020-01-02"),DateUtils.max(LocalDate.parse("2020-01-02"),LocalDate.parse("2020-01-01")));
+    assertEquals(LocalDate.parse("2003-07-15"),DateUtils.max(LocalDate.parse("2003-01-10"),LocalDate.parse("2003-07-15")));
+    assertEquals(LocalDate.parse("2003-07-15"),DateUtils.max(LocalDate.parse("2003-07-15"),LocalDate.parse("2003-01-10")));
   }
 }
