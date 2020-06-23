2e85f5f3c64cde19acb336a3383a9c08298c6995
Janusz Kowal <jkowalrz@gmail.com>
2020-06-23 17:23:42 +0200

    Pair tests

--- src/main/java/org/blacksmith/commons/string/Pair.java
+++ src/main/java/org/blacksmith/commons/string/Pair.java
@@ -9,7 +9,7 @@ public class Pair<L,R> {
     this.right = right;
   }
 
-  public static <T> Pair<T,T> of(T left, T right) {
+  public static <L,R> Pair<L,R> of(L left, R right) {
     return new Pair<>(left, right);
   }
 
---
+++ src/test/java/org/blacksmith/commons/string/PairTest.java
@@ -0,0 +1,28 @@
+package org.blacksmith.commons.string;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PairTest {
+  @Test
+  public void integerPairTest() {
+    Pair<Integer,Integer> pair = Pair.of(1,2);
+    assertEquals(1,pair.getLeft());
+    assertEquals(2,pair.getRight());
+  }
+
+  @Test
+  public void stringPairTest() {
+    Pair<String,String> pair = Pair.of("ala","kot");
+    assertEquals("ala",pair.getLeft());
+    assertEquals("kot",pair.getRight());
+  }
+
+  @Test
+  public void combinedPairTest() {
+    Pair<String,Integer> pair = Pair.of("ala",25);
+    assertEquals("ala",pair.getLeft());
+    assertEquals(25,pair.getRight());
+  }
+}
