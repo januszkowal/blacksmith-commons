dce167cd0fb34eb9214a9d31003d7284e38b84e3
Janusz Kowal <jkowalrz@gmail.com>
2020-06-23 17:32:09 +0200

    check string length tests

--- src/test/java/org/blacksmith/commons/arg/ArgCheckerTest.java
+++ src/test/java/org/blacksmith/commons/arg/ArgCheckerTest.java
@@ -40,4 +40,22 @@ class ArgCheckerTest {
     assertThrows(IllegalArgumentException.class,()-> ArgChecker.notEmpty(new String[]{},"empty array"));
     assertThrows(IllegalArgumentException.class,()-> ArgChecker.notEmpty(new String[]{},()->"empty array"));
   }
+
+  @Test
+  public void checkStringLength() {
+    ArgChecker.checkStringLength("abcde",5);
+    ArgChecker.checkStringLength("abcde",5,"Invalid length");
+    ArgChecker.checkStringLength("abcde",5,()->"Invalid length");
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength(null,5));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength(null,5,"Invalid length"));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength(null,5,()->"Invalid length"));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("",5));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("",5,"Invalid length"));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("",5,()->"Invalid length"));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("abcd",5));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("abcd",5,"Invalid length"));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("abcd",5,()->"Invalid length"));assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("abcdef",5));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("abcdef",5,"Invalid length"));
+    assertThrows(IllegalArgumentException.class,()->ArgChecker.checkStringLength("abcdef",5,()->"Invalid length"));
+  }
 }
