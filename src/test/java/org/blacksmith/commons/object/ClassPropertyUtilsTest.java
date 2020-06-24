80e4cebd67b9b260588b15e71700edc23bacea6c
Janusz Kowal <jkowalrz@gmail.com>
2020-06-24 11:30:13 +0200

    Class property utils tests

--- src/test/java/org/blacksmith/commons/object/ClassPropertyUtilsTest.java
+++ src/test/java/org/blacksmith/commons/object/ClassPropertyUtilsTest.java
@@ -4,17 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.blacksmith.commons.string.Pair;
 import org.junit.jupiter.api.Test;
 
 class ClassPropertyUtilsTest {
-
-  @Test
-  void getPropertyFromMethod() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-    Dummy1 p = new Dummy1("Qaz","Pou");
-    assertEquals("Qaz",ClassPropertyUtils.getProperty(p, "s1"));
-    assertEquals("Pou",ClassPropertyUtils.getProperty(p, "s2"));
-  }
 
   @Test
   void getPropertyField() throws IllegalAccessException {
@@ -29,5 +23,16 @@ class ClassPropertyUtilsTest {
     Pair<String,Integer> p = Pair.of("Qaz",8);
     assertEquals("Qaz",ClassPropertyUtils.getProperty(p,"left"));
     assertEquals(8,ClassPropertyUtils.getProperty(p,"right"));
+  }
+
+  @Test
+  void getPropertyFromMethod() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    Method ms1 = Dummy2.class.getMethod("getS1");
+    assertEquals("s1",ClassPropertyUtils.getPropertyFromMethod(ms1));
+    assertTrue(ClassPropertyUtils.isGetter(ms1));
+    assertFalse(ClassPropertyUtils.isSetter(ms1));
+    Method ms2 = Dummy2.class.getMethod("setS1",String.class);
+    assertFalse(ClassPropertyUtils.isGetter(ms2));
+    assertTrue(ClassPropertyUtils.isSetter(ms2));
   }
 }
