a53aa838b9adb66c326cfdcacd6ce3503777a623
Janusz Kowal <jkowalrz@gmail.com>
2021-04-20 21:22:49 +0200

    ArgUtils class

---
+++ src/main/java/org/blacksmith/commons/arg/ArgUtils.java
@@ -0,0 +1,22 @@
+package org.blacksmith.commons.arg;
+
+
+public class ArgUtils {
+  private ArgUtils() {}
+
+  public static <T> T firstNotNull(T arg1, T arg2) {
+    return arg1 != null ? arg1 : arg2;
+  }
+
+  public static <T> T firstNotNull(T arg1, T arg2, T arg3) {
+    return arg1 != null ? arg1 : firstNotNull(arg2, arg3);
+  }
+
+  public static <T> T firstNotNull(T arg1, T arg2, T arg3, T arg4) {
+    return arg1 != null ? arg1 : firstNotNull(arg2, arg3, arg4);
+  }
+
+  public static <T> T firstNotNull(T arg1, T arg2, T arg3, T arg4, T arg5) {
+    return arg1 != null ? arg1 : firstNotNull(arg2, arg3, arg4, arg5);
+  }
+}
