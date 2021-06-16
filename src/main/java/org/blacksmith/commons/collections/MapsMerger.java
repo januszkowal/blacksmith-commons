0bc482410653df2b9acf48b58325034f3694c81e
Janusz Kowal <jkowalrz@gmail.com>
2021-06-16 13:01:38 +0200

    DateUtils.yearFractionBetween added

--- src/main/java/org/blacksmith/commons/collections/MapsMerger.java
+++ src/main/java/org/blacksmith/commons/collections/MapsMerger.java
@@ -2,22 +2,37 @@ package org.blacksmith.commons.collections;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 
 public class MapsMerger {
-  private MapsMerger() {}
-  public static <K1, K2, T> Map<K1, Map<K2, T>> mergeTwoLevelMaps(Map<K1, Map<K2, T>> map1, Map<K1, Map<K2, T>> map2,
-      BiFunction<T, T, T> merger) {
-    Map<K1, Map<K2, T>> result = new HashMap<>();
-    map1.forEach((kId, eId) -> {
-      var dateMap = result.computeIfAbsent(kId, map -> new HashMap<>());
-      eId.forEach(dateMap::put);
+
+  private MapsMerger() {
+  }
+
+  /* creates new map as a result of merge */
+  public static <K1, K2, V, M extends Map<K1, Map<K2, V>>> M mergeTwoLevelMaps(M m1, M m2,
+      BinaryOperator<V> merger) {
+    Map<K1, Map<K2, V>> result = new HashMap<>();
+    m1.forEach((m1k1, m1v1) -> {
+      var dateMap = result.computeIfAbsent(m1k1, map -> new HashMap<>());
+      m1v1.forEach(dateMap::put);
     });
 
-    map2.forEach((kId, eId) -> {
-      var dateMap = result.computeIfAbsent(kId, map -> new HashMap<>());
-      eId.forEach((kDate, eCr) -> dateMap.merge(kDate, eCr, merger));
+    m2.forEach((m2k1, m2v1) -> {
+      var dateMap = result.computeIfAbsent(m2k1, map -> new HashMap<>());
+      m2v1.forEach((m2k2, m2v2) -> dateMap.merge(m2k2, m2v2, merger));
     });
-    return result;
+    return (M) result;
+  }
+
+  public static <K1, K2, V, M extends Map<K1, Map<K2, V>>> BinaryOperator<M> twoLevelMapsMerger(
+      BinaryOperator<V> mergeFunction) {
+    return (m1, m2) -> {
+      m2.forEach((m2k1, m2v1) -> {
+        var dateMap = m1.computeIfAbsent(m2k1, map -> new HashMap<>());
+        m2v1.forEach((m2k2, m2v2) -> dateMap.merge(m2k2, m2v2, mergeFunction));
+      });
+      return m1;
+    };
   }
 }
