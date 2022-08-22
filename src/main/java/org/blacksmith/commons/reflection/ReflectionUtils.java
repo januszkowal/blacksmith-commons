6ae18c6098c759eccc05c431e0687a3bc5196ac3
Janusz Kowal <janusz.kowal-extern@dbschenker.com>
2022-08-22 11:58:50 +0200

    java 17

--- src/main/java/org/blacksmith/commons/reflection/ReflectionUtils.java
+++ src/main/java/org/blacksmith/commons/reflection/ReflectionUtils.java
@@ -4,20 +4,25 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 public class ReflectionUtils {
-
-  public static ParameterizedType findByRawType(Type[] genericInterfaces, Class<?> expectedRawType) {
-    for (Type type : genericInterfaces) {
-      if (type instanceof ParameterizedType) {
-        ParameterizedType parametrized = (ParameterizedType) type;
-        if (expectedRawType.equals(parametrized.getRawType())) {
-          return parametrized;
-        }
-      }
-    }
-    throw new RuntimeException();
+  private ReflectionUtils() {
   }
 
-  public static ParameterizedType findByRawType(Class<?> clazz, Class<?> expectedRawType) {
-    return findByRawType(clazz.getGenericInterfaces(), expectedRawType);
+  public static Class<?> getGenericParameterType(Class<?> clazz, Class<?> expectedRawType) {
+    return getGenericParameterType(clazz, expectedRawType, 0);
+  }
+
+  public static Class<?> getGenericParameterType(Class<?> clazz, Class<?> expectedRawType, int parameterNo) {
+    Type[] genericInterfaces = clazz.getGenericInterfaces();
+    ParameterizedType type = findByRawType(genericInterfaces, expectedRawType);
+    return (Class<?>) type.getActualTypeArguments()[parameterNo];
+  }
+
+  private static ParameterizedType findByRawType(Type[] genericInterfaces, Class<?> expectedRawType) {
+    for (Type type : genericInterfaces) {
+      if (type instanceof ParameterizedType parametrized && expectedRawType.getClass().equals(parametrized.getRawType().getClass())) {
+        return parametrized;
+      }
+    }
+    throw new IllegalArgumentException("Invalid class definition");
   }
 }
--- src/test/java/org/blacksmith/commons/tree/TreeSearchTest.java
+++ src/test/java/org/blacksmith/commons/tree/TreeSearchTest.java
@@ -7,36 +7,36 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class TreeSearchTest {
+class TreeSearchTest {
 
-  private static final int NODES_COUNT = 111;
+    private static final int NODES_COUNT = 111;
 
-  private static final BTreeNode<Long> root = BTreeNode.of(0L);
+    private static final BTreeNode<Long> root = BTreeNode.of(0L);
 
-  @BeforeAll
-  public static void setUp() {
-    new TreeFactory<>(TreeFactory.createLongSupplier()).populateTotal(root, NODES_COUNT, 3);
-  }
+    @BeforeAll
+    static void setUp() {
+        new TreeFactory<>(TreeFactory.createLongSupplier()).populateTotal(root, NODES_COUNT, 3);
+    }
 
-  @Test
-  public void testSingleFound() {
-    assertThat(Optional.ofNullable(root.findDescendantWith(3L)).map(TreeNode::getData).orElse(null)).isEqualTo(3);
-  }
+    @Test
+    void testSingleFound() {
+        assertThat(Optional.ofNullable(root.findDescendantWith(3L)).map(TreeNode::getData).orElse(null)).isEqualTo(3L);
+    }
 
-  @Test
-  public void testSingleNotFound() {
-    assertThat(Optional.ofNullable(root.findDescendantWith(NODES_COUNT + 1L)).map(TreeNode::getData).orElse(null)).isNull();
-  }
+    @Test
+    void testSingleNotFound() {
+        assertThat(Optional.ofNullable(root.findDescendantWith(NODES_COUNT + 1L)).map(TreeNode::getData)).isNotPresent();
+    }
 
-  @Test
-  public void testSingleFound2() {
-    Long[] expected = {3L};
-    assertThat(Arrays.stream(root.findDescendantsArrayWith(3L)).map(TreeNode::getData).toArray()).containsExactly(expected);
-  }
+    @Test
+    void testSingleFound2() {
+        Long[] expected = {3L};
+        assertThat(Arrays.stream(root.findDescendantsArrayWith(3L)).map(TreeNode::getData).toArray()).containsExactly(expected);
+    }
 
-  @Test
-  public void testPredicateFound() {
-    Long[] expected = {7L, 8L, 9L};
-    assertThat(root.findDescendantsWith((d) -> d > 6 && d < 10).stream().map(TreeNode::getData).sorted().toArray()).containsExactly(expected);
-  }
+    @Test
+    void testPredicateFound() {
+        Long[] expected = {7L, 8L, 9L};
+        assertThat(root.findDescendantsWith((d) -> d > 6 && d < 10).stream().map(TreeNode::getData).sorted().toArray()).containsExactly(expected);
+    }
 }
